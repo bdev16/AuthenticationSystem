17d11df2397a497cb90499893c6b2505248f9c51
Bruno Antonio <brunoantonio.0016@gmail.com>
2026-01-27 22:06:08 -0300

    Create AppDbContext Class
    This class represents the connection to relational database

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+using AuthenticationSystem.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthenticationSystem.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
