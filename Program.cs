0c322f386857d777a6168bbff5690f23e8204a61
Bruno Antonio <brunoantonio.0016@gmail.com>
2026-01-27 22:24:29 -0300

    Create an instances of the DbContext
    Based on the informations contained in the AppDbContext class

--- Program.cs
+++ Program.cs
@@ -1,8 +1,17 @@
+using AuthenticationSystem.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+var SupaBaseConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("SupaBaseConnection"));
+});
 
 var app = builder.Build();
 
