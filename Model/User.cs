204bcc3cce249f6f09038621972269a6b58de447
Bruno Antonio <brunoantonio.0016@gmail.com>
2026-01-27 21:43:32 -0300

    Create user class

---
+++ Model/User.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthenticationSystem.Model
+{
+    public class User
+    {
+        public string email { get; set; }
+        public string senha {get; set; }
+    }
+}
