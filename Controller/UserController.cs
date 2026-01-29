26c1ed9ecf8d01a43f5bfc96f143a02b0a2e5b6b
Bruno Antonio <brunoantonio.0016@gmail.com>
2026-01-29 20:50:29 -0300

    Create controller for User class

---
+++ Controller/UserController.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AuthenticationSystem.Data;
+using AuthenticationSystem.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthenticationSystem.Controller
+{
+    [ApiController]
+    [Route("UserController")]
+    public class UserController : ControllerBase
+    {
+
+       
+    }
+}
