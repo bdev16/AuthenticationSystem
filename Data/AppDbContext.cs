84bb3aee8830b496a0ec6f4a68a9a6841f6f342e
Bruno Antonio <brunoantonio.0016@gmail.com>
2026-01-28 22:05:29 -0300

    Debug changes

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -6,8 +6,7 @@ namespace AuthenticationSystem.Data
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-        {
-            
+        {   
         }
 
         public DbSet<User> Users { get; set; }
