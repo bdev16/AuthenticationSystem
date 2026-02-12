21b0efd5e4f005eb3c5362bec07e0fa9f9c1a107
Bruno Antonio <brunoantonio.0016@gmail.com>
2026-02-12 20:06:29 -0300

    feat: Create a DTO for IdentityUser

---
+++ DTOs/UserDTO.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthenticationSystem.DTO
+{
+    public class UserDTO
+    {
+        [Required]
+        public string UserName { get; set; } = string.Empty;
+       
+        [Required]
+        [EmailAddress(ErrorMessage = "E-mail is invalid")]
+        public string Email { get; set; } = string.Empty;
+        
+    }
+}
